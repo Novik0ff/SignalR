3bcbe666b64b619a5e507b7d0381e879f94fd6bd
= <=>
2019-06-17 08:44:12 +0300

    add project Persistent Connection API step by step https://metanit.com/sharp/mvc5/16.3.php

---
+++ PersistentConMvc/PersistentConMvc/Models/Data.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PersistentConMvc.Models
+{
+    public class Data
+    {
+        public string Name { get; set; } // Имя пользователя
+        public string Message { get; set; } // Сообщение пользователя
+    }
+}
